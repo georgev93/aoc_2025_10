653d9aab2e4b4971107a3c6a53849cdcd3499f9f
georgev93 <georgeavogt93@gmail.com>
2025-12-10 22:07:13 +0530

    Worked on examples, but after running for 2 hours it didn't even crack the first line... won't finish running by end of AOC

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,7 @@ use crate::machine::{Machine, MachineShop};
 
 pub fn solve_pt1(input_file_text: &str) -> u64 {
     let mut total = 0u64;
-    let mut my_machine_shop = MachineShop::new(input_file_text);
+    let my_machine_shop = MachineShop::new(input_file_text);
 
     for mut machine in my_machine_shop.machines {
         total += machine.min_presses_to_turn_off();
@@ -16,7 +16,21 @@ pub fn solve_pt1(input_file_text: &str) -> u64 {
 }
 
 pub fn solve_pt2(input_file_text: &str) -> u64 {
-    0
+    let mut total = 0u64;
+    let mut my_machine_shop = MachineShop::new(input_file_text);
+    let mut machine_counter = 0u64;
+    let num_of_machines = my_machine_shop.machines.len();
+
+    for mut machine in my_machine_shop.machines {
+        machine_counter += 1;
+        total += machine.min_presses_to_get_joltage().unwrap();
+        println!(
+            "Machine {} of {} complete. Total is at: {}",
+            machine_counter, num_of_machines, total
+        );
+    }
+
+    total
 }
 
 pub fn solve(input_file_text: &str) -> (u64, u64) {
@@ -28,7 +42,7 @@ mod tests {
     use super::*;
 
     const EXAMPLE_PT1: u64 = 7;
-    const EXAMPLE_PT2: u64 = 0;
+    const EXAMPLE_PT2: u64 = 33;
     const ACTUAL_PT1: u64 = 390;
     const ACTUAL_PT2: u64 = 0;
 
@@ -43,8 +57,8 @@ mod tests {
     #[test]
     fn example_pts() {
         let my_file = FileParser::new("data/example.txt");
-        assert_eq!(solve_pt1(my_file.get_str()), EXAMPLE_PT1);
-        // assert_eq!(solve_pt2(my_file.get_str()), EXAMPLE_PT2);
+        // assert_eq!(solve_pt1(my_file.get_str()), EXAMPLE_PT1);
+        assert_eq!(solve_pt2(my_file.get_str()), EXAMPLE_PT2);
     }
 
     // #[test]
@@ -55,10 +69,10 @@ mod tests {
     //     assert_eq!(part_2, ACTUAL_PT2);
     // }
     //
-    #[test]
-    fn actual_pts() {
-        let my_file = FileParser::new("data/input.txt");
-        assert_eq!(solve_pt1(my_file.get_str()), ACTUAL_PT1);
-        // assert_eq!(solve_pt2(my_file.get_str()), ACTUAL_PT2);
-    }
+    // #[test]
+    // fn actual_pts() {
+    //     let my_file = FileParser::new("data/input.txt");
+    //     assert_eq!(solve_pt1(my_file.get_str()), ACTUAL_PT1);
+    //     assert_eq!(solve_pt2(my_file.get_str()), ACTUAL_PT2);
+    // }
 }
--- src/machine.rs
+++ src/machine.rs
@@ -1,13 +1,15 @@
+use nalgebra::SVector;
 use std::collections::VecDeque;
 
 type LightPos = usize;
-type Joltage = u32;
-type Button = Vec<LightPos>;
+type Joltages = SVector<isize, MAX_JOLTAGES>;
+type ButtonIdx = Vec<LightPos>;
+type Button = SVector<isize, MAX_LIGHTS_PER_BUTTON>;
 
-const MAX_LIGHTS: usize = 20;
-const MAX_LIGHTS_PER_BUTTON: usize = 20;
-const MAX_BUTTONS: usize = 20;
-const MAX_JOLTAGES: usize = 20;
+const MAX_LIGHTS: usize = 10;
+const MAX_LIGHTS_PER_BUTTON: usize = MAX_LIGHTS;
+const MAX_BUTTONS: usize = 13;
+const MAX_JOLTAGES: usize = MAX_LIGHTS;
 const MAX_MACHINES: usize = 200;
 
 enum Possibilites {
@@ -35,8 +37,9 @@ impl MachineShop {
 pub struct Machine {
     lights: Vec<bool>,
     lights_expected: Vec<bool>,
-    buttons: Vec<Button>,
-    joltage: Vec<Joltage>,
+    buttons: Vec<ButtonIdx>,
+    button_arrays: Vec<Button>,
+    joltages: Joltages,
     button_presses: u64,
 }
 
@@ -45,29 +48,154 @@ impl Machine {
         let mut buttons: Vec<Vec<LightPos>> = Vec::with_capacity(MAX_BUTTONS);
         let mut button_lights_vec: Vec<LightPos> = Vec::with_capacity(MAX_LIGHTS_PER_BUTTON);
         let mut lights: Vec<bool> = Vec::with_capacity(MAX_LIGHTS);
+        let mut current_joltage: String = String::new();
+        let mut joltages: Joltages = SVector::<isize, MAX_LIGHTS>::zeros();
+        let mut joltage_idx: usize = 0;
 
-        for c in input.chars() {
+        let mut char_iter = input.chars();
+
+        // Lights
+        for c in &mut char_iter {
             match c {
                 '.' => lights.push(false),
                 '#' => lights.push(true),
+                ']' => break,
+                _ => {}
+            }
+        }
+
+        // Buttons
+        for c in &mut char_iter {
+            match c {
                 '(' => button_lights_vec.clear(),
                 ')' => buttons.push(button_lights_vec.clone()),
                 '0'..='9' => button_lights_vec.push((c as u32 - '0' as u32) as usize),
+                '{' => break,
+                _ => {}
+            }
+        }
+
+        // Joltage
+        for c in &mut char_iter {
+            match c {
+                '0'..='9' => current_joltage.push(c),
+                ',' | '}' => {
+                    joltages[joltage_idx] =
+                        current_joltage.parse().expect("Failed to parse joltage");
+                    joltage_idx += 1;
+                    current_joltage.clear();
+                }
                 _ => {}
             }
         }
 
         buttons.sort_unstable_by_key(|b| b.len() as isize);
-
-        // lights.reverse();
+        let mut button_arrays: Vec<Button> = Vec::with_capacity(buttons.len());
+        for button in &buttons {
+            button_arrays.push(Self::generate_button_arr(button));
+        }
 
         Self {
             lights_expected: vec![false; lights.len()],
             lights,
             buttons,
-            joltage: vec![],
+            button_arrays,
+            joltages,
             button_presses: 0,
         }
+    }
+
+    fn generate_button_arr(idx_button: &ButtonIdx) -> Button {
+        let mut ret_val = SVector::<isize, MAX_LIGHTS>::zeros();
+        for idx in idx_button {
+            ret_val[*idx] = 1;
+        }
+        ret_val
+    }
+
+    pub fn min_presses_to_get_joltage(&mut self) -> Option<u64> {
+        let mut min_presses: Option<u64> = None;
+
+        let mut idx_to_remove: Vec<usize> = Vec::new();
+        for (idx, button) in &mut self.button_arrays.iter().enumerate() {
+            // Check to ensure a button press won't make it go negative
+            if self.joltages.iter().zip(button).any(|(j, b)| b > j) {
+                idx_to_remove.push(idx);
+            }
+        }
+
+        idx_to_remove.reverse();
+
+        for idx in idx_to_remove {
+            self.button_arrays.remove(idx);
+        }
+
+        if self.button_arrays.is_empty() {
+            if self.joltages.abs().max() == 0 {
+                return Some(0);
+            } else {
+                // println!("Reached the end but no cigar");
+                return None;
+            }
+        }
+
+        let mut max_button_presses = 0isize;
+        let mut button = self.button_arrays[0];
+        while !self.button_arrays.is_empty() {
+            button = self.button_arrays.pop().unwrap();
+            // println!("Buttons available: {}", self.button_arrays.len());
+
+            // Check to ensure a button press won't make it go negative
+            if self.joltages.iter().zip(&button).any(|(j, b)| b > j) {
+                // println!("CAUGHT ONE");
+                continue;
+            }
+
+            max_button_presses = button
+                .component_mul(&self.joltages)
+                .iter()
+                .filter(|&&x| x != 0)
+                .min()
+                .copied()
+                .unwrap_or(0);
+
+            if max_button_presses != 0 {
+                // println!("Max presses found: {}", max_button_presses);
+                break;
+            }
+        }
+
+        for presses in (0..=max_button_presses).rev() {
+            let mut clone_machine = self.clone();
+            if (clone_machine.joltages - button * presses).min() < 0 {
+                dbg!(max_button_presses);
+                dbg!(&clone_machine.joltages);
+                dbg!(presses);
+                dbg!(button);
+                panic!();
+            }
+            clone_machine.joltages -= button * presses;
+            if let Some(mut local_min) = clone_machine.min_presses_to_get_joltage() {
+                // println!(
+                //     "Found ONE! on obj with {} buttons left",
+                //     self.button_arrays.len()
+                // );
+                local_min += presses as u64;
+                if min_presses.is_none() {
+                    // println!("Storing local min: {}", local_min);
+                    min_presses = Some(local_min);
+                }
+                if local_min < min_presses.unwrap() {
+                    min_presses = Some(local_min);
+                }
+            }
+        }
+        // if let some(presses) = min_presses {
+        //     // println!("RETURNING {}", presses);
+        // }
+        //     println!("Returning none");
+        // }
+        min_presses
     }
 
     pub fn min_presses_to_turn_off(&mut self) -> u64 {
@@ -89,10 +217,10 @@ impl Machine {
                 return successful_button_presses;
             }
 
-            // SAFETY!!!
-            if pass_counter > 100000000 {
-                panic!("TOO MANY POSSIBILITIES");
-            }
+            // // SAFETY!!!
+            // if pass_counter > 100000000 {
+            //     panic!("TOO MANY POSSIBILITIES");
+            // }
         }
         dbg!(pass_counter);
         panic!("Not possible!!!")
